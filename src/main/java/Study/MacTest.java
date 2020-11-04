75a6980d4586d2877e23376ec158b6fbbe63553e
comma <981638035@qq.com>
2020-11-05 06:39:32 +0800

    mac test

---
+++ src/main/java/Study/MacTest.java
@@ -0,0 +1,12 @@
+package Study;
+
+/**
+ * @Author: comma
+ * @Date: 2020/11/5 06:38
+ * @version: 1.0
+ */
+public class MacTest {
+    public static void main(String[] args) {
+        System.out.println("mac test");
+    }
+}
