b6fa4f2a06e4b0eb98b4079c753a4f0964332d24
comma1990 <981638035@qq.com>
2020-11-04 12:22:36 +0800

    Initial commit

---
+++ src/main/java/Study/Demo.java
@@ -0,0 +1,18 @@
+package Study;
+
+import java.util.ArrayList;
+
+/**
+ * @Author: comma
+ * @Date: 2020/11/4 11:05
+ * @version: 1.0
+ */
+public class Demo {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            i++;
+            System.out.println(i);
+            ArrayList arrayList=new ArrayList();
+        }
+    }
+}
---
+++ src/main/java/Study/Student.java
@@ -0,0 +1,48 @@
+package Study;
+
+/**
+ * @Author: comma
+ * @Date: 2020/11/4 11:07
+ * @version: 1.0
+ */
+
+/*
+* 一、构造器快捷方式：Alt+insert
+* */
+
+public class Student {
+    private String name;
+    private int age;
+
+    public Student() {
+    }
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
